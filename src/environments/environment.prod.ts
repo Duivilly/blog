5ff5291310ae357d88a8b84f981c2f87b3d7c9ee
Duivilly Brito <db@icomp.ufam.edu.br>
2021-08-26 09:46:22 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyA8HEn4Lg0bwjHdYif4ntWZTg-BhkrUpIs",
-    authDomain: "blog-bbccc.firebaseapp.com",
-    projectId: "blog-bbccc",
-    storageBucket: "blog-bbccc.appspot.com",
-    messagingSenderId: "593620156334",
-    appId: "1:593620156334:web:676dc94ebe9fdaefb6123f",
-    measurementId: "G-JX8E4WYS2V"
+
   }
 };
